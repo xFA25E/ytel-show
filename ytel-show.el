b4b955bd084e516887202d75579f04438959c991
xFA25E <55886566+xFA25E@users.noreply.github.com>
2020-11-15 20:45:58 +0100

    better error handling, reorganized code

--- ytel-show.el
+++ ytel-show.el
@@ -24,7 +24,25 @@
 
 ;;; Code:
 
+
+;;;; IMPORTS
+
 (require 'ytel)
+
+
+;;;; TYPES
+
+(cl-defstruct (ytel-show--video (:constructor ytel-show--video-create)
+                                (:copier nil))
+  title thumbnail-data description published
+  length views likes dislikes author-id)
+
+(cl-defstruct (ytel-show--author (:constructor ytel-show--author-create)
+                                 (:copier nil))
+  name thumbnail-data subs)
+
+
+;;;; VARIABLES
 
 (defvar ytel-show--video-fields
   (string-join
@@ -48,51 +66,54 @@
   (let ((map (make-sparse-keymap)))
     (define-key map "n" #'ytel-show-next-video)
     (define-key map "p" #'ytel-show-previous-video)
+    (define-key map "G" #'ytel-show-reload-video-data)
     map)
-  "Keymap for `ytel-mode'.")
+  "Keymap for `ytel-show-mode'.")
+
+
+;;;; FUNCTIONS
+
+
+;;;;; MISC
 
 (defun ytel-show--current-video-id ()
   (aref ytel-show--video-ids ytel-show--index))
 
-(cl-defstruct (ytel-show-video (:constructor ytel-show-video--create)
-                               (:copier nil))
-  (title "" :read-only t)
-  (thumbnail-data nil :read-only t)
-  (description "" :read-only t)
-  (published 0 :read-only t)
-  (length 0 :read-only t)
-  (views 0 :read-only t)
-  (likes 0 :read-only t)
-  (dislikes 0 :read-only t)
-  (author-id "" :read-only t))
-
-(cl-defstruct (ytel-show-author (:constructor ytel-show-author--create)
-                                (:copier nil))
-  (name "" :read-only t)
-  (thumbnail-data "" :read-only t)
-  (subs "" :read-only t))
-
-(defun ytel-show--video-query (id)
+(defun ytel-show--query-video (id)
   (ytel--API-call (concat "videos/" id) `(("fields" ,ytel-show--video-fields))))
 
-(defun ytel-show--best-thumbnail (thumbnails)
-  (cl-labels ((compare
-               ((h1 . u1) (h2 . u2))
-               (and (<= h1 h2) (not (string-match-p (regexp-quote ytel-invidious-api-url) u2))))
-              (vars (e) (let-alist e (cons .height .url)))
-              (pred (acc elm) (if (compare (vars acc) (vars elm)) elm acc)))
-    (seq-reduce #'pred (subseq thumbnails 1) (aref thumbnails 0))))
+
+;;;;; THUMBNAILS
 
-(defun ytel-show--thumbnail-data (thumbnail)
-  (cl-flet ((furl (u) (if (string-match-p (rx bos "http" (? "s") ":") u) u (concat "https:" u)))
-            (scale (width height)
-                   (when (or (< ytel-show-image-max-width width)
-                             (< ytel-show-image-max-height height))
-                     (list :scale (min (/ ytel-show-image-max-width width)
-                                       (/ ytel-show-image-max-height height))))))
-    (let-alist thumbnail
-      (unless (string-empty-p .url)
-        (let ((buffer (url-retrieve-synchronously (furl .url))))
+(defun ytel-show--filter-thumbnails (thumbnails)
+  (cl-flet ((valid-thumbnail-p (thumbnail)
+             (let-alist thumbnail
+               (and (numberp .width) (< 0 .width)
+                    (numberp .height) (< 0 .height)
+                    (stringp .url) (not (string-empty-p .url))
+                    (string-match-p (rx bos "http" (? "s") "://") .url)
+                    (not (string-match-p (regexp-quote ytel-invidious-api-url) .url))))))
+    (seq-filter #'valid-thumbnail-p thumbnails)))
+
+(defun ytel-show--select-thumbnail (thumbnails)
+  (cl-case (length thumbnails)
+    (0 nil)
+    (1 (elt thumbnails 0))
+    (t
+     (cl-labels ((height (e) (cdr (assq 'height e)))
+                 (cmp (e1 e2) (<= (height e1) (height e2)))
+                 (pred (acc elm) (if (cmp acc elm) elm acc)))
+       (seq-reduce #'pred (subseq thumbnails 1) (aref thumbnails 0))))))
+
+(defun ytel-show--thumbnail-load-data (thumbnail)
+  (when thumbnail
+    (cl-flet ((scale (width height)
+               (when (or (< ytel-show-image-max-width width)
+                         (< ytel-show-image-max-height height))
+                 (list :scale (min (/ ytel-show-image-max-width width)
+                                   (/ ytel-show-image-max-height height))))))
+      (let-alist thumbnail
+        (let ((buffer (url-retrieve-synchronously .url)))
           (unwind-protect
               (let ((data (with-current-buffer buffer
                             (goto-char (point-min))
@@ -101,81 +122,53 @@
                 (apply #'create-image data nil t (scale .width .height)))
             (kill-buffer buffer)))))))
 
-(defun ytel-show--cache (id)
-  (let-alist (ytel-show--video-query id)
-    (let ((video (ytel-show-video--create
-                  :title .title
-                  :thumbnail-data
-                  (ytel-show--thumbnail-data
-                   (ytel-show--best-thumbnail .videoThumbnails))
-                  :description .description
-                  :published .published
-                  :length .lengthSeconds
-                  :views .viewCount
-                  :likes .likeCount
-                  :dislikes .dislikeCount
-                  :author-id .authorId))
-          (author (ytel-show-author--create
-                   :name .author
-                   :thumbnail-data
-                   (ytel-show--thumbnail-data
-                    (ytel-show--best-thumbnail .authorThumbnails))
-                   :subs .subCountText)))
-      (setf (gethash .videoId ytel-show--videos-cache) video
-            (gethash .authorId ytel-show--authors-cache) author)
-      `((video . ,video) (author . ,author)))))
+(defun ytel-show--process-thumbnails (thumbnails)
+  (thread-last thumbnails
+    ytel-show--filter-thumbnails
+    ytel-show--select-thumbnail
+    ytel-show--thumbnail-load-data))
 
-(defun ytel-show--video-data (id)
-  (if-let ((video (gethash id ytel-show--videos-cache)))
-      `((video . ,video)
-        (author . ,(gethash (ytel-show-video-author-id video)
-                            ytel-show--authors-cache)))
-    (ytel-show--cache id)))
+
+;;;;; UPDATE
 
-(defun ytel-show--insert-data (data)
-  (let* ((video (cdr (assq 'video data)))
-         (title (ytel-show-video-title video))
-         (thumbnail-data (ytel-show-video-thumbnail-data video))
-         (description (ytel-show-video-description video))
-         (published (ytel-show-video-published video))
-         (length (ytel-show-video-length video))
-         (views (ytel-show-video-views video))
-         (likes (ytel-show-video-likes video))
-         (dislikes (ytel-show-video-dislikes video))
-         (author-id (ytel-show-video-author-id video))
+(defun ytel-show--update-video (video query-response)
+  (let-alist query-response
+    (setf
+     (ytel-show--video-thumbnail-data video) (ytel-show--process-thumbnails .videoThumbnails)
+     (ytel-show--video-title video) .title
+     (ytel-show--video-description video) .description
+     (ytel-show--video-published video) .published
+     (ytel-show--video-length video) .lengthSeconds
+     (ytel-show--video-views video) .viewCount
+     (ytel-show--video-likes video) .likeCount
+     (ytel-show--video-dislikes video) .dislikeCount
+     (ytel-show--video-author-id video) .authorId))
+  video)
 
-         (author (cdr (assq 'author data)))
-         (name (ytel-show-author-name author))
-         (author-thumbnail-data (ytel-show-author-thumbnail-data author))
-         (subs (ytel-show-author-subs author)))
-    (insert title "\n")
-    (when thumbnail-data (insert-image thumbnail-data))
-    (insert "\n")
-    (insert (format "Length: %s | Published: %s | Views: %s | Likes: %s | Dislikes: %s\n"
-                    length published views likes dislikes)
-            author-id " " name " " subs "\n"
-            description "\n")
-    (when author-thumbnail-data (insert-image author-thumbnail-data))
-    (insert "\n")))
+(defun ytel-show--update-author (author query-response)
+  (let-alist query-response
+    (setf
+     (ytel-show--author-thumbnail-data author) (ytel-show--process-thumbnails .authorThumbnails)
+     (ytel-show--author-name author) .author
+     (ytel-show--author-subs author) .subCountText))
+  author)
 
-(defun ytel-show-revert-buffer (&rest _)
-  (interactive)
-  (let ((inhibit-read-only t))
-    (erase-buffer)
-    (ytel-show--insert-data (ytel-show--video-data (ytel-show--current-video-id)))
-    (goto-char (point-min))))
+(defun ytel-show--update-cache (id)
+  (let* ((query-response (ytel-show--query-video id))
+         (video (ytel-show--update-video
+                 (or (gethash id ytel-show--videos-cache)
+                     (puthash id (ytel-show--video-create)
+                              ytel-show--videos-cache))
+                 query-response))
+         (author-id (ytel-show--video-author-id video))
+         (author (ytel-show--update-author
+                  (or (gethash author-id ytel-show--authors-cache)
+                      (puthash author-id (ytel-show--author-create)
+                               ytel-show--authors-cache))
+                  query-response)))
+    `((video . ,video) (author . ,author))))
 
-(defun ytel-show (video-ids index)
-  (interactive (if (not (derived-mode-p 'ytel-mode))
-                   (error "Not in the ytel buffer")
-                 (list (vconcat (seq-map #'ytel-video-id ytel-videos))
-                       (1- (line-number-at-pos)))))
-  (switch-to-buffer (get-buffer-create "*ytel-show*"))
-  (unless (derived-mode-p 'ytel-show-mode)
-    (ytel-show-mode))
-  (setq ytel-show--video-ids video-ids ytel-show--index index)
-  (ytel-show-revert-buffer))
-
+
 (defun ytel-show--update-index (add &optional cycle)
   (let ((new-index (+ add ytel-show--index))
         (length (length ytel-show--video-ids)))
@@ -186,6 +179,55 @@
       (setq ytel-show--index new-index))
      (t (message "Border!")))))
 
+
+;;;;; DATA
+(defun ytel-show--video-data (id)
+  (if-let* ((video (gethash id ytel-show--videos-cache))
+            (author (gethash (ytel-show--video-author-id video)
+                             ytel-show--authors-cache)))
+      `((video . ,video) (author . ,author))
+    (ytel-show--update-cache id)))
+
+
+;;;;; DRAW
+
+(defun ytel-show--draw-data (data)
+  (let* ((video (cdr (assq 'video data)))
+         (title (ytel-show--video-title video))
+         (thumbnail-data (ytel-show--video-thumbnail-data video))
+         (description (ytel-show--video-description video))
+         (published (ytel-show--video-published video))
+         (length (ytel-show--video-length video))
+         (views (ytel-show--video-views video))
+         (likes (ytel-show--video-likes video))
+         (dislikes (ytel-show--video-dislikes video))
+         (author-id (ytel-show--video-author-id video))
+
+         (author (cdr (assq 'author data)))
+         (name (ytel-show--author-name author))
+         (author-thumbnail-data (ytel-show--author-thumbnail-data author))
+         (subs (ytel-show--author-subs author)))
+    (insert (format "%s\n" title))
+    (when thumbnail-data
+      (insert-image thumbnail-data)
+      (insert "\n"))
+    (insert (format "Length: %s | Published: %s | Views: %s | Likes: %s | Dislikes: %s\n%s %s %s\n%s\n"
+                    length published views likes dislikes
+                    author-id name subs description))
+    (when author-thumbnail-data
+      (insert-image author-thumbnail-data)
+      (insert "\n"))))
+
+
+;;;; COMMANDS
+
+(defun ytel-show-revert-buffer (&rest _)
+  (interactive)
+  (let ((inhibit-read-only t))
+    (erase-buffer)
+    (ytel-show--draw-data (ytel-show--video-data (ytel-show--current-video-id)))
+    (goto-char (point-min))))
+
 (defun ytel-show-next-video (&optional cycle)
   (interactive "P")
   (ytel-show--update-index 1 cycle)
@@ -194,6 +236,22 @@
 (defun ytel-show-previous-video (&optional cycle)
   (interactive "P")
   (ytel-show--update-index -1 cycle)
+  (ytel-show-revert-buffer))
+
+(defun ytel-show-reload-video-data ()
+  (interactive)
+  (ytel-show--update-cache (ytel-show--current-video-id))
+  (ytel-show-revert-buffer))
+
+(defun ytel-show (video-ids index)
+  (interactive (if (not (derived-mode-p 'ytel-mode))
+                   (error "Not in the ytel buffer")
+                 (list (vconcat (seq-map #'ytel-video-id ytel-videos))
+                       (1- (line-number-at-pos)))))
+  (switch-to-buffer (get-buffer-create "*ytel-show*"))
+  (unless (derived-mode-p 'ytel-show-mode)
+    (ytel-show-mode))
+  (setq ytel-show--video-ids video-ids ytel-show--index index)
   (ytel-show-revert-buffer))
 
 (define-derived-mode ytel-show-mode special-mode "Ytel-Show"
